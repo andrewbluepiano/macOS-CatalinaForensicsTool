a40d62a71764464d834cf0469b8510abeb525882
Andrew Afonso <andrewbluepiano@hotmail.com>
2021-12-01 20:23:02 -0500

    Changed disclaimer.

--- CatalinaExporter/main.m
+++ CatalinaExporter/main.m
@@ -17,7 +17,7 @@ int main(int argc, const char * argv[]) {
     [[NSBundle mainBundle] loadAppleScriptObjectiveCScripts];
     NSAlert *alert = [[NSAlert alloc] init];
     [alert setMessageText:@"Disclaimer & Info"];
-    [alert setInformativeText:@"This app requires full disk access. Please enable it for the App in System Preferences.\n\nThis app is for easy information gathering only. It is intended to be run on a booted image of a logged in system, to which you have access to the password for the administrators password. \n\nAlso, as this entire application is a fight against Apple's normal application controls, as well as... Catalina being Catalina. Don't be shocked if somethings wonky. My MacBook Pro has crashed more timed developing this than in the 4 years I have owned it. It seems to be tied to file operations, especially those using uncompiled AppleScript. It was never as a result of running the app itself, so it shouldnt be an issue, but if you begin to add your own functions, dont be suprised if you experience similar things."];
+    [alert setInformativeText:@"This app requires full disk access. Please enable it for the App in System Preferences.\n\nThis app is for easy information gathering only. It is intended to be run on a booted image of a logged in system, to which you have access to the password for the administrators password. \n\nAlso, as this entire application is a fight against Apple's normal application controls. Don't be shocked if somethings wonky."];
     [alert addButtonWithTitle:@"Ok"];
     [alert runModal];
     ArtifactFinder *stuff = [[NSClassFromString(@"ArtifactFinder") alloc] init];
